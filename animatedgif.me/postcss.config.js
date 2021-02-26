1b516fa49f51af8f11c8e70690e60be96d03e753
tony-ramos-pina <tony.ramos.pina@gmail.com>
2021-02-26 12:32:07 +0000

    ...

--- animatedgif.me/postcss.config.js
+++ animatedgif.me/postcss.config.js
@@ -14,13 +14,14 @@ let environment = {
   ]
 }
 
-if (process.env.RAILS_ENV === "production") {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: ['./app/**/*.html.erb', './app/helpers/**/*.rb'],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
+// if (process.env.RAILS_ENV === "production") {
+//   environment.plugins.push(
+//     require('@fullhuman/postcss-purgecss')({
+//       content: ['./app/**/*.html.erb', './app/helpers/**/*.rb'],
+//       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+//     })
+//   )
+// }
+
 
 module.exports = environment
